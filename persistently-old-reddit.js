bb5796a3f9b16160bbcedd72e0beefa84dc2d63c
Nikos Nikolaou <destdpd@gmail.com>
2024-12-25 19:15:08 +0100

    Update persistently-old-reddit.js

--- persistently-old-reddit.js
+++ persistently-old-reddit.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            Persistently Old Reddit
-// @version         0.1.0
+// @version         0.1.1
 // @description     Provides a consistent experience of the old Reddit UI throughout browsing.
 // @author          PolyMegos (https://github.com/polymegos)
 // @namespace       https://github.com/polymegos/persistently-old-reddit
@@ -19,8 +19,8 @@
 (function() {
     'use strict';
 
-    // Check if current URL is not already old.reddit.com
-    if (!location.hostname.startsWith('old.')) {
+    // Check if current URL is not already old.reddit.com and not a /media page
+    if (!location.hostname.startsWith('old.') && !location.pathname.startsWith('/media')) {
         const newUrl = location.href.replace(/^(https?:\/\/)(www\.)?reddit\.com/, '$1old.reddit.com');
         location.replace(newUrl);
     }
